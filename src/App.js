c78cecb7683e089b53c5009ce5f249deb5551d37
“Abby” <abdullah16aug@gmail.com>
2022-07-31 21:45:29 +0530

    cryptoTracker

--- src/App.js
+++ src/App.js
@@ -1,33 +1,62 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Coin.css';
+import Coin from './Coin';
 
-import "./App.css";
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false
 function App() {
-
-  const [coin,setCoins]= useState([])
-
-
-
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false'
       )
-      .then((response) => {
-        setCoins(response.data);
-     
-      }).catch((error) => {
-        console.log(error);
-      },[]);
-  });
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
-    <div className="App">
-      <h1>Apii</h1>
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+           
+          />
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+export default  App
